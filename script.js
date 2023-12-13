1ceb95cec15cacee4b73cb61169de64820ddad80
Jordan W. Toujouse <146011829+jordan2juice@users.noreply.github.com>
2023-12-12 19:13:38 -0600

    added script for scrolling effect

--- script.js
+++ script.js
@@ -3,3 +3,23 @@ copyright = document.querySelector("#copyright");
 copyright.appendChild(document.createTextNode(new Date().getFullYear()));
 
 
+const the_animation = document.querySelectorAll("#animation")
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            entries,target.classList.add('scoll-animations')
+        } else {
+            entry.target.classList.remove('scroll-animation')
+        }
+    })
+},
+
+{ threshold: 0.5
+};
+//
+for (let i=0; i<the_animation.length; i++) {
+    const elements = the_animation[i];
+
+    observer.observe(elements);
+}

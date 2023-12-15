270bada0d65d0d9747937c808f52115b3f5009e9
Jordan W. Toujouse <146011829+jordan2juice@users.noreply.github.com>
2023-12-14 19:14:21 -0600

    saved changes

--- script.js
+++ script.js
@@ -2,10 +2,10 @@
 copyright = document.querySelector("#copyright");
 copyright.appendChild(document.createTextNode(new Date().getFullYear()));
 
-function changeQuoteToAqua () {
-    let quote = document.querySelector("#quote");
-    quote.style.color = "aqua";
-    console.log ("in change quote")
+function changeQuoteToAqua() {
+  let quote = document.querySelector("#quote");
+  quote.style.color = "aqua";
+  console.log("in change quote");
 }
 
-changeQuoteToAqua ()
+changeQuoteToAqua();
